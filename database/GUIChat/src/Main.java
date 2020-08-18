37e9fefe3a03370a1bd42580ce83f60797ddc107
jmanga2020 <jack706manga@gmail.com>
2020-08-18 23:17:56 +0300

    first commit

---
+++ database/GUIChat/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main 
+{
+    public static void main(String [] args)
+    {
+        MainFrame mainFrame = new MainFrame(); //create the MainFrame object
+        Thread thread  = new Thread(mainFrame); //run in a new thread
+        thread.start(); //start the thread
+    }
+}
